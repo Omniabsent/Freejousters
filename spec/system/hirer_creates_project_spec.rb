6cb0ce8e22651c8b4aca3db24d4d31a14af23214
Julia <julia.nihil@gmail.com>
2021-10-13 05:53:26 -0300

    melhorado teste de cadastro de projeto, criado teste de busca de projeto

--- spec/system/hirer_creates_project_spec.rb
+++ spec/system/hirer_creates_project_spec.rb
@@ -22,17 +22,10 @@ describe 'Hirer creates project' do
 
   it 'and it shows in their "my projects" list' do
     user = User.create!(email: 'captain@flint.com', password: 'asdfasdf', role: 'hirer')
+    Project.create!(title:'Saquear o Urca de Lima', description:'Procuro piratas qualificados para atacar e roubar o ouro dos espanhóis', wanted_skills:'Saber operar canhão, lutar com pistola e espada, ler mapas', max_pay: 99999, expiration_date:'01-01-1700', user: user)
 
     login_as user, scope: :user
     visit root_path
-    click_on 'Criar novo projeto'
-    fill_in 'Nome do projeto', with: 'Saquear o Urca de Lima'
-    fill_in 'Descrição', with: 'Procuro piratas qualificados para atacar e roubar o ouro dos espanhóis'
-    fill_in 'Skills necessárias', with: 'Saber operar canhão, lutar com pistola e espada, ler mapas'
-    fill_in 'Valor máximo oferecido [hora]', with: 99999
-    fill_in 'Data limite para início', with: '01-01-1700'
-    click_on 'Enviar'
-    click_on 'Home'
     click_on 'Meus projetos'
 
     expect(page).to have_content('Saquear o Urca de Lima')
@@ -43,14 +36,8 @@ describe 'Hirer creates project' do
 
     login_as user, scope: :user
     visit root_path
-    click_on 'Criar novo projeto'
-    fill_in 'Nome do projeto', with: 'Saquear o Urca de Lima'
-    fill_in 'Descrição', with: 'Procuro piratas qualificados para atacar e roubar o ouro dos espanhóis'
-    fill_in 'Skills necessárias', with: 'Saber operar canhão, lutar com pistola e espada, ler mapas'
-    fill_in 'Valor máximo oferecido [hora]', with: 99999
-    fill_in 'Data limite para início', with: '01-01-1700'
-    click_on 'Enviar'
-    click_on 'Home'
+    Project.create!(title:'Saquear o Urca de Lima', description:'Procuro piratas qualificados para atacar e roubar o ouro dos espanhóis', wanted_skills:'Saber operar canhão, lutar com pistola e espada, ler mapas', max_pay: 99999, expiration_date:'01-01-1700', user: user)
+
     click_on 'Projetos disponíveis'
 
     expect(page).to_not have_content('Saquear o Urca de Lima')
@@ -60,17 +47,8 @@ describe 'Hirer creates project' do
     user = User.create!(email: 'captain@flint.com', password: 'asdfasdf', role: 'hirer')
     professional = User.create!(email: 'edward@teach.com', password: 'asdfasdf', role: 'hireable')
     user_profile = UserProfile.create(name: 'Edward Teach', social_name: 'Blackbeard', birth_date: '01/01/1700', major: 'Captain', bio: 'The Scourge Of The Seven Seas', experience: 'Over a decade as the captain of the Queen Anne', picture: 'blackbeard.jpg', user: professional)
+    Project.create!(title:'Saquear o Urca de Lima', description:'Procuro piratas qualificados para atacar e roubar o ouro dos espanhóis', wanted_skills:'Saber operar canhão, lutar com pistola e espada, ler mapas', max_pay: 99999, expiration_date:10.days.from_now, user: user)
 
-    login_as user, scope: :user
-    visit root_path
-    click_on 'Criar novo projeto'
-    fill_in 'Nome do projeto', with: 'Saquear o Urca de Lima'
-    fill_in 'Descrição', with: 'Procuro piratas qualificados para atacar e roubar o ouro dos espanhóis'
-    fill_in 'Skills necessárias', with: 'Saber operar canhão, lutar com pistola e espada, ler mapas'
-    fill_in 'Valor máximo oferecido [hora]', with: 99999
-    fill_in 'Data limite para início', with: '01-01-2100'
-    click_on 'Enviar'
-    click_on 'Logout'
 
     login_as professional, scope: :user
     visit root_path
