a1757797675dbdd98b6c232942d5fd640e5f1006
Julia <julia.nihil@gmail.com>
2021-10-16 05:05:34 -0300

    usuário vê propostas de profissionais

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,6 +1,6 @@
 class UserProfile < ApplicationRecord
   belongs_to :user
-  has_many :proposal
-  
+  has_many :proposals
+
   validates :name, :social_name, :birth_date, :major, :bio, :experience, :picture, presence: true
 end
--- spec/system/hirer_can_see_proposals_to_their_project_spec.rb
+++ spec/system/hirer_can_see_proposals_to_their_project_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'hirer sees proposals to their project' do
   it 'successfully' do
     professional = User.create!(email: 'edward@teach.com', password: 'asdfasdf', role: 'hireable')
-    user_profile = UserProfile.create(name: 'Edward Teach', social_name: 'Blackbeard', birth_date: '01/01/1700', major: 'Captain', bio: 'The Scourge Of The Seven Seas', experience: 'Over a decade as the captain of the Queen Anne', picture: 'blackbeard.jpg', user: professional)
+    user_profile = UserProfile.create!(name: 'Edward Teach', social_name: 'Blackbeard', birth_date: '01/01/1700', major: 'Captain', bio: 'The Scourge Of The Seven Seas', experience: 'Over a decade as the captain of the Queen Anne', picture: 'blackbeard.jpg', user: professional)
     user = User.create!(email: 'captain@flint.com', password: 'asdfasdf', role: 'hirer')
     project = Project.create!(title:'Salvar Nassau', description:'Busco pessoas interessadas em recuperar Nassau do domínio de Woodes Rogers', wanted_skills:'Ser capaz de lutar', max_pay: 50, expiration_date:10.days.from_now, user: user)
     Proposal.create!(presentation:'Sou um dos piratas mais temidos do novo mundo e considero Nassau meu verdadeiro lar', charges: 100, week_hours: 10, total_hours: 20, project: project, user: professional)
