7a2f1e92e22f7842e3259ad72b25b62d14f99277
Julia <julia.nihil@gmail.com>
2021-10-16 04:12:58 -0300

    criados testes de aprovação

---
+++ spec/system/hirer_can_see_proposals_to_their_project_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe 'hirer sees proposals to their project' do
+  it 'successfully' do
+    professional = User.create!(email: 'edward@teach.com', password: 'asdfasdf', role: 'hireable')
+    user_profile = UserProfile.create(name: 'Edward Teach', social_name: 'Blackbeard', birth_date: '01/01/1700', major: 'Captain', bio: 'The Scourge Of The Seven Seas', experience: 'Over a decade as the captain of the Queen Anne', picture: 'blackbeard.jpg', user: professional)
+    user = User.create!(email: 'captain@flint.com', password: 'asdfasdf', role: 'hirer')
+    project = Project.create!(title:'Salvar Nassau', description:'Busco pessoas interessadas em recuperar Nassau do domínio de Woodes Rogers', wanted_skills:'Ser capaz de lutar', max_pay: 50, expiration_date:10.days.from_now, user: user)
+    Proposal.create!(presentation:'Sou um dos piratas mais temidos do novo mundo e considero Nassau meu verdadeiro lar', charges: 100, week_hours: 10, total_hours: 20, project: project, user: professional)
+
+    login_as user, scope: :user
+    visit root_path
+    click_on 'Meus projetos'
+    click_on 'Salvar Nassau'
+
+    expect(page).to have_content('Candidatos:')
+    expect(page).to have_content("Proposta de: #{user_profile.social_name}")
+    expect(page).to have_link('Aprovar')
+    expect(page).to have_link('Rejeitar')
+  end
+
+  it 'and approves one candidate' do
+    professional = User.create!(email: 'edward@teach.com', password: 'asdfasdf', role: 'hireable')
+    user_profile = UserProfile.create(name: 'Edward Teach', social_name: 'Blackbeard', birth_date: '01/01/1700', major: 'Captain', bio: 'The Scourge Of The Seven Seas', experience: 'Over a decade as the captain of the Queen Anne', picture: 'blackbeard.jpg', user: professional)
+    user = User.create!(email: 'captain@flint.com', password: 'asdfasdf', role: 'hirer')
+    project = Project.create!(title:'Salvar Nassau', description:'Busco pessoas interessadas em recuperar Nassau do domínio de Woodes Rogers', wanted_skills:'Ser capaz de lutar', max_pay: 50, expiration_date:10.days.from_now, user: user)
+    Proposal.create!(presentation:'Sou um dos piratas mais temidos do novo mundo e considero Nassau meu verdadeiro lar', charges: 100, week_hours: 10, total_hours: 20, project: project, user: professional)
+
+    login_as user, scope: :user
+    visit root_path
+    click_on 'Meus projetos'
+    click_on 'Salvar Nassau'
+    click_on "Aprovar #{user_profile.social_name}"
+
+    expect(page).to have_content("Proposta de: #{user_profile.social_name} aprovada")
+  end
+end
