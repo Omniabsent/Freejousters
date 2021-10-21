339e0497a7114482f8fc14efee123e47592aa579
Julia <julia.nihil@gmail.com>
2021-10-21 12:03:14 -0300

    hirer não pode mais ver proposal que tenha sido cancelado antes dele aprovar, testes passando

--- spec/system/hirer_can_see_proposals_to_their_project_spec.rb
+++ spec/system/hirer_can_see_proposals_to_their_project_spec.rb
@@ -134,7 +134,7 @@ describe 'hirer sees proposals to their project' do
     expect(page).to have_content("Motivo do cancelamento: Because reasons")
   end
 
-  it 'and the the hirer can see pressional\'s justification' do
+  it 'and the hirer can see professional\'s justification' do
     professional = User.create!(email: 'edward@teach.com', password: 'asdfasdf', role: 'hireable')
     professional_profile = UserProfile.create!(name: 'Edward Teach', social_name: 'Blackbeard', birth_date: '01/01/1700', major: 'Captain', bio: 'The Scourge Of The Seven Seas', experience: 'Over a decade as the captain of the Queen Anne', picture: 'blackbeard.jpg', user: professional)
     user = User.create!(email: 'captain@flint.com', password: 'asdfasdf', role: 'hirer')
@@ -150,6 +150,25 @@ describe 'hirer sees proposals to their project' do
     expect(page).to have_content("Proposta de #{professional_profile.social_name} cancelled")
     expect(page).to have_content("Motivo do cancelamento: Because reasons")
   end
+
+  it 'and the hirer cannot see proposal cancelled before approval' do
+    professional = User.create!(email: 'edward@teach.com', password: 'asdfasdf', role: 'hireable')
+    professional_profile = UserProfile.create!(name: 'Edward Teach', social_name: 'Blackbeard', birth_date: '01/01/1700', major: 'Captain', bio: 'The Scourge Of The Seven Seas', experience: 'Over a decade as the captain of the Queen Anne', picture: 'blackbeard.jpg', user: professional)
+    user = User.create!(email: 'captain@flint.com', password: 'asdfasdf', role: 'hirer')
+    user_profile = UserProfile.create!(name: 'James MacGraw', social_name: 'Flint', birth_date: '01/01/1700', picture: 'flint.jpg', user: user)
+    project = Project.create!(title:'Salvar Nassau', description:'Busco pessoas interessadas em recuperar Nassau do domínio de Woodes Rogers', wanted_skills:'Ser capaz de lutar', max_pay: 50, expiration_date:10.days.from_now, user: user)
+    Proposal.create!(presentation:'Sou um dos piratas mais temidos do novo mundo e considero Nassau meu verdadeiro lar', charges: 100, week_hours: 10, total_hours: 20, project: project, user: professional, status: 'cancelled')
+
+    login_as user, scope: :user
+    visit root_path
+    click_on 'Meus projetos'
+    click_on 'Salvar Nassau'
+
+    expect(page).to_not have_content("Proposta de: #{professional_profile.social_name}")
+    expect(page).to_not have_content("Proposta de #{professional_profile.social_name} cancelled")
+    expect(page).to_not have_content("Motivo do cancelamento: Because reasons")
+  end
+
 
   it 'and the candidate cannot cancel proposal more than 3 days after it was approved' do
     professional = User.create!(email: 'edward@teach.com', password: 'asdfasdf', role: 'hireable')
